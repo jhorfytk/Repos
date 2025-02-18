7570cb07b580913f763b2e32317c6698b0a468ba
jhorfytk <jhorfyreyes@gmail.com>
2025-02-18 14:00:55 -0400

    Add files via upload

---
+++ ejercicio x.cpp
@@ -0,0 +1,20 @@
+// Calculadora de Promedio//
+
+#include<iostream>
+using namespace std;
+
+int main (){
+
+int a,b,c,d,e,Promedio=0;
+
+cout<<"valor de a:" ; cin>>a;
+cout<<"valor de b:" ; cin>>b;
+cout<<"valor de c:" ; cin>>c;
+cout<<"valor de d:" ; cin>>d;
+cout<<"valor de e:" ; cin>>e;
+Promedio= (a+b+c+d+e)/5;
+cout<< "El Promedio es:"<<Promedio<< endl;
+
+
+return 0;
+}
